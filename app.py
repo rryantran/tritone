72e26dd0756cf7799a9d8149f4b84cc6cfae460f
ryantran <113406848+rryantran@users.noreply.github.com>
2023-07-26 21:54:28 -0500

    configure shell context

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from app import app
+from app import app, db
+from app.models import Article
 
-if __name__ == "__main__":
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Article': Article}
+
+
+if __name__ == '__main__':
     app.run()
