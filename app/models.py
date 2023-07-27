a30a591860537648762fd8b5e2a951f453b092cc
ryantran <113406848+rryantran@users.noreply.github.com>
2023-07-26 22:06:47 -0500

    remove author

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from app import db
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), index=True, unique=True)
-    author = db.Column(db.String(32))
     link = db.Column(db.String(256), index=True, unique=True)
     desc = db.Column(db.String(512))
     pubdate = db.Column(db.DateTime)
