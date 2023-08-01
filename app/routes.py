73176fd0f820b617b5ff4da2953726ae781ea8ce
ryantran <113406848+rryantran@users.noreply.github.com>
2023-07-31 23:32:26 -0500

    add pagination

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 import feedparser
 from app import app, db
 from app.models import Article
-from sqlalchemy import desc
-from flask import render_template
+from flask import render_template, request
 from time import mktime
 from datetime import datetime, timezone
 
@@ -14,7 +13,8 @@ def to_datetime(entry_date):
 @app.route('/')
 @app.route('/home')
 def index():
-    feeds = ['https://pitchfork.com/rss/news/']
+    feeds = ['https://pitchfork.com/rss/news/', 'https://www.nme.com/news/music/feed',
+             'https://i-d.vice.com/en_uk/rss/topic/music', 'https://www.rollingstone.com/music/music-news/feed/']
 
     for feed in feeds:
         parsed_feed = feedparser.parse(feed)
@@ -28,6 +28,8 @@ def index():
                 db.session.add(new_article)
                 db.session.commit()
 
-    articles = Article.query.order_by(desc(Article.pubdate)).all()
+    page = request.args.get('page', 1, type=int)
+    articles = Article.query.order_by(Article.pubdate.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False).items
 
     return render_template('index.html', title='Home', articles=articles)
