b48f8c1fb4ee2ba00a8dfedefe9f9a18ae593242
ryantran <113406848+rryantran@users.noreply.github.com>
2023-08-07 21:49:15 -0500

    add author

--- app/models.py
+++ app/models.py
@@ -4,20 +4,22 @@ from app import db
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
+    author = db.Column(db.String(128))
     link = db.Column(db.String(256), unique=True)
     pubdate = db.Column(db.DateTime)
     guid = db.Column(db.String(256), unique=True)
 
     def __repr__(self):
-        return f'Title <{self.title}>, Date Published <{self.pubdate}>'
+        return f'Title <{self.title}>, Author(s) <{self.author}>'
 
 
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
+    author = db.Column(db.String(128))
     link = db.Column(db.String(256), unique=True)
     pubdate = db.Column(db.DateTime)
     guid = db.Column(db.String(256), unique=True)
 
     def __repr__(self):
-        return f'Title <{self.title}>, Date Published <{self.pubdate}>'
+        return f'Title <{self.title}>, Author(s) <{self.author}>'
