4da3a895648f9c9c4a5ba0f8797e10a658373e4d
ryantran <113406848+rryantran@users.noreply.github.com>
2023-08-10 22:17:46 -0500

    add login methods to user model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from app import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True)
     password_hash = db.Column(db.String(128))
