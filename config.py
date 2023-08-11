de89b56398d84be315ba7322e067003e1e826635
ryantran <113406848+rryantran@users.noreply.github.com>
2023-08-10 21:35:51 -0500

    add forms config

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config():
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL') or f'sqlite:///{os.path.join(basedir, "app.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
