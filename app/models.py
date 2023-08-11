d5c4be6d29902bdd6460ec17fdbb1439d39be84d
ryantran <113406848+rryantran@users.noreply.github.com>
2023-08-10 22:12:22 -0500

    add password methods to user model

--- app/models.py
+++ app/models.py
@@ -1,9 +1,21 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True)
     password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f'User <{self.email}>'
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
