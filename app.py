5e1baf273596f0f28bf46df62e8e0c07e19543e8
ryantran <113406848+rryantran@users.noreply.github.com>
2023-08-10 22:33:47 -0500

    edit shell context

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from app import app, db
-from app.models import Review, Article
+from app.models import User, Review, Article
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Review': Review, 'Article': Article}
+    return {'db': db, 'User': User, 'Review': Review, 'Article': Article}
 
 
 if __name__ == '__main__':
