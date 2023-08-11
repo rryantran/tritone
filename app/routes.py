3a573c3dee6e8eff928081925a20dbf83a2dc908
ryantran <113406848+rryantran@users.noreply.github.com>
2023-08-10 22:32:22 -0500

    edit login route

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 import feedparser
 from app import app, db
 from app.forms import LoginForm
-from app.models import Review, Article
+from app.models import User, Review, Article
 from flask import render_template, url_for, request, redirect
+from flask_login import current_user, login_user, logout_user
 from urllib.parse import urlparse
 from time import mktime
 from datetime import datetime, timezone
@@ -17,12 +18,29 @@ def to_datetime(entry_date):
 def index():
     return render_template('index.html', title='Home')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
+
     if form.validate_on_submit():
-        return redirect('/home')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember.data)
+        return redirect(url_for('index'))
+
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 
 @app.route('/reviews')
 def reviews():
