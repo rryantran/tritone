9e2c777eff05f1c8a2a98ce5cdc25c07f83a93af
ryantran <113406848+rryantran@users.noreply.github.com>
2023-08-15 19:52:50 -0500

    set up moment

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+moment = Moment(app)
 
 from app import routes, forms, models
