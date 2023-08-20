5b3c084260e973fd56d72cb00a00553e4d609a3b
ryantran <113406848+rryantran@users.noreply.github.com>
2023-08-20 15:25:48 -0500

    fix migration issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from config import Config
+from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_moment import Moment
 
+convention = {
+    "ix": 'ix_%(column_0_label)s', 
+    "uq": "uq_%(table_name)s_%(column_0_name)s", 
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db, render_as_batch=True)
 login = LoginManager(app)
 moment = Moment(app)
 
